09e1576143cd9e4a5083900c27f2e2112c857154
admin <bosko15082004@gmail.com>
2024-12-06 15:58:07 +0300

    Changed division, inversion and multiplying. Almost done

--- Lab_2_Polynom/src/Main.java
+++ Lab_2_Polynom/src/Main.java
@@ -4,55 +4,80 @@ import java.nio.file.Path;
 
 public class Main {
 
-    public static int add(int a, int b) {
+    public static long add(long a, long b) {
         return a ^ b;
     }
 
-    public static int multiply(int a, int b, int mod) {
-        int result = 0;
-        while (b != 0) {
-            if ((b & 1) != 0) {
-                result ^= a;
-            }
-            b >>= 1;
-            a <<= 1;
-            if ((a & (1 << (Integer.toBinaryString(mod).length() - 1))) != 0) {
-                a ^= mod;
-            }
+    public static long divide (long num, long denom, long[] quot) {
+        long nD = getPolynomDegree(num);
+        long dD = getPolynomDegree(denom);
+        quot[0] = 0;
+        while (nD >= dD) {
+            quot[0] ^= (1L << (nD - dD));
+            num ^= denom << (nD - dD);
+            nD = getPolynomDegree(num);
         }
+        return num;
+    }
+
+    public static long getPolynomDegree(long poly) {
+        long degree = -1;
+        while (poly != 0) {
+            poly >>= 1;
+            degree++;
+        }
+        return degree;
+    }
+
+    public static long multiply(long poly1, long poly2, long polynom) {
+        long irreduciblePolyDegree = getPolynomDegree(polynom);
+        long result = 0;
+        while (poly1 != 0) {
+            if ((poly1 & 1) == 1) {
+                result ^= poly2;
+            }
+            poly2 <<= 1;
+            if ((poly2 & (1L << irreduciblePolyDegree)) != 0) {
+                poly2 ^= polynom;
+            }
+            poly1 >>= 1;
+        }
+
         return result;
     }
-
-    public static int modInverse(int a, int mod) {
-        int r0 = mod, r1 = a;
-        int t0 = 0, t1 = 1;
-
+    public static long inverse(long poly, long polynom) {
+        long r0 = polynom;
+        long r1 = poly;
+        long t = 0, s = 1;
+        long[] quotient = new long[1];
         while (r1 != 0) {
-            int q = r0 / r1;
-            int temp = r0 % r1;
+            long buf = divide(r0, r1, quotient);
             r0 = r1;
-            r1 = temp;
-
-            temp = t0 ^ multiply(q, t1, mod);
-            t0 = t1;
-            t1 = temp;
+            r1 = buf;
+            buf = t ^ multiply(quotient[0], s, polynom);
+            t = s;
+            s = buf;
         }
 
-        if (r0 != 1) {
-            throw new ArithmeticException("Обратный элемент не существует");
+        return t;
+    }
+
+    public static long mod(long poly, long irreduciblePolynomial) {
+        long polyDegree = getPolynomDegree(poly);
+        long irreducibleDegree = getPolynomDegree(irreduciblePolynomial);
+
+        while (polyDegree >= irreducibleDegree) {
+            poly ^= irreduciblePolynomial << (polyDegree - irreducibleDegree);
+            polyDegree = getPolynomDegree(poly);
         }
 
-        return t0;
+        return poly;
     }
 
-    public static int divide(int a, int b, int mod) {
-        int inverse = modInverse(b, mod);
-        return multiply(a, inverse, mod);
-    }
 
-    public static int power(int a, int n, int mod) {
-        int result = 1;
-        int base = a;
+    public static long power(long a, long n, long mod) {
+        long result = 1;
+        long base = a;
 
         while (n > 0) {
             if ((n & 1) != 0) {
@@ -65,23 +90,23 @@ public class Main {
         return result;
     }
 
-    public static int parsePolynomial(String polynomialString) {
+    public static long parsePolynomial(String polynomialString) {
         String reversed = new StringBuilder(polynomialString).reverse().toString();
-        return Integer.parseInt(reversed, 2);
+        return Long.parseLong(reversed, 2);
     }
 
-    public static String toBinaryString(int polynomial) {
-        String binary = Integer.toBinaryString(polynomial);
+    public static String toBinaryString(long polynomial) {
+        String binary = Long.toBinaryString(polynomial);
         return new StringBuilder(binary).reverse().toString();
     }
 
-    public static String formatPolynomial(int poly) {
+    public static String formatPolynomial(long poly) {
         StringBuilder result = new StringBuilder();
         boolean isFirstTerm = true;
-        int degree = Integer.toBinaryString(poly).length() - 1;
+        long degree = Long.toBinaryString(poly).length() - 1;
 
         while (poly != 0) {
-            if ((poly & (1 << degree)) != 0) {
+            if ((poly & (1L << degree)) != 0) {
                 if (!isFirstTerm) {
                     result.append(" + ");
                 }
@@ -95,7 +120,7 @@ public class Main {
                 isFirstTerm = false;
             }
             degree--;
-            poly &= ~(1 << (degree + 1));
+            poly &= ~(1L << (degree + 1));
         }
 
         return result.length() > 0 ? result.toString() : "0";
@@ -105,30 +130,34 @@ public class Main {
         String irreduciblePolynomialStr = Files.readString(Path.of("Lab_2_Polynom/src/polynom.txt")).trim();
         String inputStr = Files.readString(Path.of("Lab_2_Polynom/src/input.txt")).trim();
 
-        int irreduciblePolynomial = parsePolynomial(irreduciblePolynomialStr);
+        long irreduciblePolynomial = parsePolynomial(irreduciblePolynomialStr);
         String[] parts = inputStr.split(" ");
-        int f1 = parsePolynomial(parts[0]);
-        int result = 0;
+        long f1 = parsePolynomial(parts[0]);
+        f1 = mod(f1, irreduciblePolynomial);
+        long result = 0;
 
         switch (parts[1]) {
             case "+":
-                int f2 = parsePolynomial(parts[2]);
+                long f2 = parsePolynomial(parts[2]);
+                f2 = mod(f2, irreduciblePolynomial);
                 result = add(f1, f2);
                 break;
             case "*":
                 f2 = parsePolynomial(parts[2]);
+                f2 = mod(f2, irreduciblePolynomial);
                 result = multiply(f1, f2, irreduciblePolynomial);
                 break;
             case "/":
                 f2 = parsePolynomial(parts[2]);
-                result = divide(f1, f2, irreduciblePolynomial);
+                f2 = mod(f2, irreduciblePolynomial);
+                result = divide(f1, f2, new long[]{irreduciblePolynomial});
                 break;
-            case "^":
-                int power = Integer.parseInt(parts[2]);
+            case "ˆ":
+                long power = Long.parseLong(parts[2]);
                 result = power(f1, power, irreduciblePolynomial);
                 break;
-            case "-1":
-                result = modInverse(f1, irreduciblePolynomial);
+            case "ˆ-1":
+                result = inverse(f1, irreduciblePolynomial);
                 break;
             default:
                 throw new IllegalArgumentException("Неизвестная операция: " + parts[1]);
@@ -138,4 +167,5 @@ public class Main {
         Files.writeString(Path.of("Lab_2_Polynom/src/output.txt"), resultBinary
                 + '\n' + formatPolynomial(result));
     }
+
 }
